88b80a6e7cfbbda09c3e8b6743639d8f0b2568ce
Pedro Soto <sotope06@gmail.com>
2020-10-26 17:45:52 -0600

    Commited

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import database from './db';
 const server = Server.instance;
 const db = database.instance;
 
-<<<<<<< HEAD
+
 db.open();
 server.start();
-=======
+
 server.start();
 console.log('pedro puto')
->>>>>>> 157f1b8b783e79c20d7a1d476c0f0658972b8bb2
+
