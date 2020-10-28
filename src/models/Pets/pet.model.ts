cf372b7d99752eccf6bd042e5cefdb47977687e6
Pedro Soto <sotope06@gmail.com>
2020-10-27 18:04:35 -0600

    Updated

--- src/models/Pets/pet.model.ts
+++ src/models/Pets/pet.model.ts
@@ -3,38 +3,23 @@ import uniqueValidator from 'mongoose-unique-validator';
 import IPet from "./pet.interface";
 
 const petSchema = new Schema ({
-<<<<<<< HEAD
-    Owner: {
-        type: Schema.Types.ObjectId,
-       // required: [true, 'Se requiere la identificación del dueño']
-       default: null
-=======
     owner: {
         type: Schema.Types.ObjectId,
         required: [true, 'El ID del dueño es requerido']
->>>>>>> d19df192428eb35f57ad4eff0d9e7e07c6ed193c
+
     },
     name: {
         type: String,
         required: [true, 'El nombre es requerido']
     },
     age: {
-        type: Number,
-<<<<<<< HEAD
-        required: [true, 'Se requiere la edad']
-=======
+        type: Schema.Type.Number,
         required: [true, 'La edad es requerida']
->>>>>>> d19df192428eb35f57ad4eff0d9e7e07c6ed193c
     },
     breed: {
         type: String,
         required: [true, 'La raza es requerida <3']
     },
-<<<<<<< HEAD
-    char: {
-        type: String,
-        default: null
-=======
     chars: {
         type: String,
         default: null,
@@ -42,7 +27,6 @@ const petSchema = new Schema ({
     alive: {
         type: Boolean,
         default: true
->>>>>>> d19df192428eb35f57ad4eff0d9e7e07c6ed193c
     }
 }, {
     timestamps: {
@@ -50,12 +34,6 @@ const petSchema = new Schema ({
     },
     versionKey: false
 });
-
-<<<<<<< HEAD
-petSchema.plugin(uniqueValidator, {message: 'El {PATH} {VALUE} ya existe'});
-
-export const Pet = model<IPet>('Pet',petSchema);
-=======
 petSchema.index({createdAt: 1});
 petSchema.index({alive: 1});
 petSchema.index({name: 1});
@@ -66,4 +44,4 @@ petSchema.index({name: 1, alive: 1, owner: 1}, {unique: true, partialFilterExpre
 petSchema.plugin(uniqueValidator, {message: 'El {PATH} {VALUE} ya existe'});
 
 export const Pet = model<IPet>('Pet', petSchema);
->>>>>>> d19df192428eb35f57ad4eff0d9e7e07c6ed193c
+
--- src/models/Pets/pet.routes.ts
+++ src/models/Pets/pet.routes.ts
@@ -4,30 +4,66 @@ import { filters } from "../../middlewares/filters";
 import petController from "./pet.controller";
 import { imageIncluded } from "../../middlewares/files";
 
-import { petStructure } from "./pet.middleware";
-import DB from "../../db";
-import { Pet } from "./pet.model";
-
-const db = DB.instance;
-
 const PetRoutes = Router();
 
 PetRoutes.get('/',(req, res ) => { res.send("From PetRoutes")});
 
-PetRoutes.post('',[petStructure,imageIncluded],async (req: any, res: Response) => {
-
-    const session = await db.db.startSession();
-
-    session.startTransaction();
+PetRoutes.post('',[imageIncluded],async (req: any, res: Response) => {
 
     try {
 
         const newPet = req.body;        
 
-        const [pet] = await petController.save(newPet,session);
+        const pet = await petController.save(newPet);
 
-        await session.commitTransaction();
-        session.endSession();
+        return res.status(200).json(GenericResponse.success(pet, 'Se guardó el registro correctamente'));
+
+    } catch (error) {
+        return res.status(400).json(GenericResponse.error(error, 'Error al guardar la mascota'));
+    }
+});
+
+PetRoutes.get('/list', [filters], async (req: any, res: Response) => {
+
+    try {
+
+        if(req.query.search) {
+            req.query.filters.name = req.query.search;
+        }
+        if(req.query.unit) {
+            req.query.filters.unit = req.query.unit;
+        }
+
+        const [pets, count] = await petController.getAll(req.query.limit, req.query.skip, req.query.filters, req.query.all, req.query.populate);
+
+        let pagination = {};
+
+        if(!req.query.all) {
+            const pages = Math.ceil((count / req.query.limit ));
+
+            pagination = {
+                perPage: req.query.limit,
+                pages
+            };
+        }
+        
+        return res.status(200).json(GenericResponse.success({
+            pets,
+            count,
+            ...pagination
+        }, "Se obtuvieron las mascotas correctamente"));
+    } catch (error) {
+        return res.status(400).json(GenericResponse.error(error, "Error al obtener las mascotas"));
+    }
+});
+
+PetRoutes.put('/:petId', [imageIncluded], async (req: any, res: Response) => {
+
+    try {
+
+        let pet = req.body;
+
+        pet = await petController.update(pet, {_id: req.params.petId});
 
         return res.status(200).json(GenericResponse.success(pet,'Mascota almacenada en la bd'));
 
